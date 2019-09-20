45ff73691b00d8d592bee76659457bc6b71fe26b
Julien <juliencaron18@gmail.com>
2019-09-20 11:38:18 +0200

    Init frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,16 @@
+import { Button, Alert } from "reactstrap";
+
+import Layout from "../components/layout";
+
+export default () => {
+	return (
+		<Layout>
+			<div>
+				<Alert color="primary">
+					Hello Project is strapi-next with Bootstrap
+				</Alert>
+				&nbsp; <Button color="primary">Hello from nextjs</Button>
+			</div>
+		</Layout>
+	);
+};
